82a798faf95f76fbeced91423d382e9a7f5eb0b8
CleeArrgghh <cleearr.canillas@lsu.edu.ph>
2023-12-09 12:20:38 +0800

    First Commit

---
+++ Readme.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tech-Ownline PC Solutions</title>
+    <style>
+        body {
+            background-color: #000;
+            color: #1ef705;
+            font-family: 'Courier New', Courier, monospace;
+        }
+        h1, h2, p, li, a {
+            text-shadow: 0 0 5px #1ef705, 0 0 10px #1ef705, 0 0 15px #1ef705, 0 0 20px #1ef705;
+            color: #fff; /* Neon white color */
+        }
+    </style>
+</head>
+<body>
+    <h1>Tech-Ownline PC Solutions</h1>
+    <p>E-commerce web app project in IT Professional Elective 3. Inspired by our last project "Tech-Ownline". Neon theme.</p>
+
+    <h2>🚀 Features</h2>
+    <ul>
+        <li>List Products</li>
+        <li>Filter products by Category</li>
+        <li>Admin Functions</li>
+        <li>Ajax Forms</li>
+        <li>Add products to Cart</li>
+        <li>Neon Theme :3</li>
+        <li>And many more.. but mainly those are the important ones</li>
+    </ul>
+
+    <h2>User guide</h2>
+    <ol>
+        <li>Download xampp</li>
+        <li>Locate the xampp folder in your machine.</li>
+        <li>Navigate to htdocs inside the folder and delete everything.</li>
+        <li>Clone this repository into the folder.</li>
+        <li>Run xampp.</li>
+        <li>Next, go to phpmyadmin localhost web to create a database with ogtech as its name.</li>
+        <li>Import all the sql data from the Tech-Ownline.sql file in the root project directory.</li>
+        <li>At the browser, open the web app url <a href="http://localhost/Tech-Ownline">http://localhost/Tech-Ownline</a></li>
+        <li>You can now use this with the credentials stated below.</li>
+    </ol>
+
+    <h2>Default Accounts</h2>
+    <p>create account first oyy. dayun refer to the localhost database.</p>
+
+    <h2>Supported Browsers:</h2>
+    <p>Browser Compatible:</p>
+    <ul>
+        <li>Chrome 35+</li>
+        <li>Firefox 31+</li>
+        <li>Safari 9+</li>
+        <li>Opera</li>
+        <li>Edge</li>
+        <li>IE 11+</li>
+    </ul>
+</body>
+</html>
---
+++ contactUs.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tech Ownline - Contact US</title>
+  <?php include "header.php"; ?>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+  <h1 class="page-title grid background-overlay" style="margin:1px;padding-top: 140px;padding-bottom: 140px;">Contact Us</h1>
+    <div class="grid" style="margin:0px 15% 0px 15%">
+        
+        <div id="contactdetailscontainer">
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">place</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:22px">Tech Ownline (registered name)</strong><br>
+                    Lasalle University Ozamiz.
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">phone</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:22px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:23px">Contact Number</strong><br>
+                    09662472098
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">email</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:20px">Tech Ownline</strong><br>
+                    techownline@gmail.com
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">alarm</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:20px">Business Hours</strong><br>
+                    Monday to Sunday: 11.00am to 8.00pm
+                </p>
+            </div>
+        </div>
+    </div>  
+
+
+  <?php require "footer.php"; ?>
+</body>
+</html>
